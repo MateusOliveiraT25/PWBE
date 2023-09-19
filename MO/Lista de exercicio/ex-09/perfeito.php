c5d23f5bdbc3db4e2dc16355a71199215f5009ce
MateusOliveiraT25 <123783867+MateusOliveiraT25@users.noreply.github.com>
2023-09-19 12:05:51 -0300

    19/09

---
+++ MO/Lista de exercicio/ex-09/perfeito.php
@@ -0,0 +1,23 @@
+<?php
+function eNumeroPerfeito($numero) {
+    $somaFatores = 1; // Inicializamos com 1 porque todo número é divisível por 1.
+
+    for ($i = 2; $i * $i <= $numero; $i++) {
+        if ($numero % $i == 0) {
+            $somaFatores += $i;
+            if ($i != ($numero / $i)) {
+                $somaFatores += ($numero / $i);
+            }
+        }
+    }
+
+    return $somaFatores == $numero;
+}
+
+echo "Números perfeitos até 1000: ";
+for ($i = 1; $i <= 1000; $i++) {
+    if (eNumeroPerfeito($i)) {
+        echo $i . " ";
+    }
+}
+?>

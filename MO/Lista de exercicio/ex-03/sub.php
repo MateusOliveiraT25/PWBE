cced5b96a7c2c16a9539185a6477337f0a7f712f
MateusOliveiraT25 <123783867+MateusOliveiraT25@users.noreply.github.com>
2023-09-19 10:03:54 -0300

    19/09

--- MO/Lista de exercicio/ex-03/sub.php
+++ MO/Lista de exercicio/ex-03/sub.php
@@ -1,19 +1,28 @@
 <?php
 // Leitura dos números
-$N1 = isset($_POST['N1']) ? floatval($_POST['N1']) : 0; //n1
-$N2 = isset($_POST['N2']) ? floatval($_POST['N2']) : 0; //n2
+$N1 = isset($_POST['N1']) ? floatval($_POST['N1']) : 0; // Primeiro número
+$N2 = isset($_POST['N2']) ? floatval($_POST['N2']) : 0; // Segundo número
 
-// Inicialização do contador
-$N3 = 0;
+// Verifica se o segundo número é zero (divisão por zero)
+if ($N2 == 0) {
+    echo "Erro: Divisão por zero.";
+} else {
+    // Inicialização do resultado
+    $resultado = 0;
 
-// Cálculo da multiplicação por somas sucessivas
-for ($i = 0; $i < $N2; $i++) {
-    $N3 += $N1;
+    // Determina qual número é o maior e qual é o menor
+    $maior = max($N1, $N2);
+    $menor = min($N1, $N2);
+
+    // Realiza a divisão por subtrações sucessivas
+    while ($maior >= $menor) {
+        $maior -= $menor;
+        $resultado++;
+    }
+
+    // Exibição do resultado
+    echo "O resultado da divisão é: $resultado";
 }
-
-// Exibição do resultado
-echo "O resultado da multiplicação é: $N3\n";
-
-
 ?>
+
 

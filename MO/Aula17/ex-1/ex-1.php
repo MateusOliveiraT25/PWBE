888c48d7aacab81b0366c659d9bda9767f3d68b7
MateusOliveiraT25 <123783867+MateusOliveiraT25@users.noreply.github.com>
2023-10-10 15:25:51 -0300

    10/10

--- MO/Aula17/ex-1/ex-1.php
+++ MO/Aula17/ex-1/ex-1.php
@@ -16,3 +16,26 @@ function verifica($n) {
 verifica($n);
 
 ?>
+/*ex6
+<?php
+$n1 = isset($_POST['n1']) ? floatval($_POST['n1']) : 0;
+$n2 = isset($_POST['n2']) ? floatval($_POST['n2']) : 0;
+
+function crescente($n1, $n2) {
+    if ($n1 < $n2) {
+        echo $n1 + $n2;
+    } else {
+        echo $n2 + $n1;
+    }
+}
+
+// Chamar a função
+crescente($n1, $n2);
+?>
+
+
+
+
+
+
+*/
